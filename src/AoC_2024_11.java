106de7d5682b474ed3defbb8e6b41117408d78ca
Cholo Galvez <240070477+cholavez-boop@users.noreply.github.com>
2025-11-18 03:04:21 +0800

    Continue AoC 2024 11

--- src/AoC_2024_11.java
+++ src/AoC_2024_11.java
@@ -1,8 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class AoC_2024_11 {
     public static void main(String[] args) {
-        String input = "2 77706 5847 9258441 0 741 883933 12";
+        File myObj = new File("/Users/Lorenzo Galvez/Documents/Work Files/JustCoding/AdventOfCoding/AoCInputs/AOC2024_11.txt");
+        // String[] splitInput = input.split("\s");
+        // ArrayList<Long> plutoStones = new ArrayList<>();
+        int numOfBlinks = 35;
+        Long reallyBigNum = 841729134425L;
+        int index = 0;
+
+        try (Scanner myReader = new Scanner(myObj)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                // System.out.println(data);
+                if (index <= 376408) {
+                    index++;
+
+                    continue;
+                }
+                ArrayList<Long> plutoStone = new ArrayList<>();
+                plutoStone.add(Long.parseLong(data));
+                for (int b = 0; b < numOfBlinks; b++) {
+                    plutoStone = blink(plutoStone);
+                }
+                reallyBigNum += plutoStone.size();
+                System.out.println("Currenlty: " + index + " -> " + reallyBigNum);
+                index++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Something went wrong.");
+            e.printStackTrace();
+        }
+        // String input = "2 77706 5847 9258441 0 741 883933 12";
         /*
          * If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
          * If the stone is engraved with a number that has an even number of digits, it is replaced by two stones.
@@ -10,27 +43,24 @@ public class AoC_2024_11 {
          * If none of the other rules apply, the stone is replaced by a new stone;
          *      the old stone's number multiplied by 2024 is engraved on the new stone.
          */
-
-        String[] splitInput = input.split("\s");
-        ArrayList<Long> plutoStones = new ArrayList<>();
          
-        for (int p = 0; p < splitInput.length; p++) {
-        plutoStones.add(Long.parseLong(splitInput[p]));
-        }
+        // for (int p = 0; p < splitInput.length; p++) {
+        //     plutoStones.add(Long.parseLong(splitInput[p]));
+        // }
 
         // ArrayList<ArrayList<Long>> stonesAbstraced = new ArrayList<>();
 
-        Long blong = 0L;
-        int numOfBlinks = 75;
+        // Long blong = 0L;
+        
         // for (int a = 0; a < plutoStones.size(); a++) {
-            ArrayList<Long> gha = new ArrayList<>();
-            gha.add(plutoStones.get(0));
-            for (int b = 0; b < numOfBlinks; b++) {
-                // plutoStones = blink(plutoStones);
-                gha = blink(gha);
-            }
+            // ArrayList<Long> gha = new ArrayList<>();
+            // gha.add(plutoStones.get(0));
+            // for (int b = 0; b < numOfBlinks; b++) {
+            //     plutoStones = blink(plutoStones);
+            //     // gha = blink(gha);
+            // }
             // stonesAbstraced.add(gha);
-            blong += gha.size();
+            // blong += gha.size();
         // }
         
         // System.out.println(plutoStones.size() + " number of pluto stones!");
@@ -38,7 +68,8 @@ public class AoC_2024_11 {
         // for (ArrayList<Long> ghu : stonesAbstraced) {
         //     blong += ghu.size();
         // }
-        System.out.println(blong + " number of pluto stones!");
+        // System.out.println(plutoStones.size() + " number of pluto stones!");
+        // writeToFile(plutoStones);
     }
 
     //Part 1
@@ -84,4 +115,18 @@ public class AoC_2024_11 {
      * 60973 TOO LOW
      * 190865 CORRECT
      */
+
+     public static void writeToFile(ArrayList<Long> plutoStones) {
+        try {
+            FileWriter w = new FileWriter("AOC2024_11.txt");
+            for (Long l : plutoStones) {
+                w.write(String.valueOf(l)+"\n");
+            }
+            w.close();
+            System.out.println("Saved to AOC2024_11.");
+        } catch (IOException e) {
+            System.out.println("Whoops!");
+            e.printStackTrace();
+        }
+     }
 }
