bd54e7a5ec6b781eb85d5c90b7810b11cbf33de2
Cholo Galvez <240070477+cholavez-boop@users.noreply.github.com>
2025-12-03 12:18:23 +0800

    AoC 2024 Day 11 Part 2 Solved

--- src/AoC_2024_11.java
+++ src/AoC_2024_11.java
@@ -15,73 +15,95 @@ public class AoC_2024_11 {
         ArrayList<Long> plutoStones = new ArrayList<>();
          
         for (int p = 0; p < splitInput.length; p++) {
-        plutoStones.add(Long.parseLong(splitInput[p]));
+            plutoStones.add(Long.parseLong(splitInput[p]));
         }
-
-        // ArrayList<ArrayList<Long>> stonesAbstraced = new ArrayList<>();
+        plutoStones.sort(null);
 
         Long blong = 0L;
         int numOfBlinks = 75;
-        // for (int a = 0; a < plutoStones.size(); a++) {
-            ArrayList<Long> gha = new ArrayList<>();
-            gha.add(plutoStones.get(0));
-            for (int b = 0; b < numOfBlinks; b++) {
-                // plutoStones = blink(plutoStones);
-                gha = blink(gha);
-            }
-            // stonesAbstraced.add(gha);
-            blong += gha.size();
-        // }
-        
-        // System.out.println(plutoStones.size() + " number of pluto stones!");
-        
-        // for (ArrayList<Long> ghu : stonesAbstraced) {
-        //     blong += ghu.size();
-        // }
+        for (Long stone : plutoStones) {
+            blong += blink(stone, numOfBlinks);
+        }
         System.out.println(blong + " number of pluto stones!");
+        // System.out.println(memo);
     }
 
     //Part 1
-    public static ArrayList<Long> blink(ArrayList<Long> plutoStones) {
-        ArrayList<Long> newPlutoStones = new ArrayList<>();
+    // public static ArrayList<Long> blink(ArrayList<Long> plutoStones) {
+    //     ArrayList<Long> newPlutoStones = new ArrayList<>();
 
-        for (int stone = 0; stone < plutoStones.size(); stone++) {
-            if (plutoStones.get(stone) == 0) {
-                newPlutoStones.add(1L);
+    //     for (int stone = 0; stone < plutoStones.size(); stone++) {
+    //         if (plutoStones.get(stone) == 0) {
+    //             newPlutoStones.add(1L);
+    //         } else {
+    //             String stoneString = String.valueOf(plutoStones.get(stone));
+    //             // System.out.println(stoneSt  ring);
+    //             if (stoneString.length()%2 == 0) {
+    //                 if (stoneString.length() == 2) {
+    //                     newPlutoStones.add(Long.parseLong(String.valueOf(stoneString.charAt(0))));
+    //                     newPlutoStones.add(Long.parseLong(String.valueOf(stoneString.charAt(1))));
+    //                 } else {
+    //                     newPlutoStones.add(Long.parseLong(stoneString.substring(0, stoneString.length()/2)));
+    //                     newPlutoStones.add(Long.parseLong(stoneString.substring(stoneString.length()/2)));
+    //                 }
+    //             } else {
+    //                 newPlutoStones.add(plutoStones.get(stone) * 2024);
+    //             }
+    //         }
+    //         // System.out.println(newPlutoStones);
+    //     }
+
+    //     return newPlutoStones;
+    // }
+
+    //Part 2
+    public static HashMap<Long, HashMap<Integer, Long>> memo = new HashMap<>();
+    public static Long blink(Long plutoStone, int blinkTime) {
+        if(memo.containsKey(plutoStone) && memo.get(plutoStone).containsKey(blinkTime)) {
+            // System.out.print(blinkTime);
+            return memo.get(plutoStone).get(blinkTime);
+        } else {
+            if (blinkTime == 0) {
+                return 1L;
             } else {
-                String stoneString = String.valueOf(plutoStones.get(stone));
-                // System.out.println(stoneSt  ring);
-                if (stoneString.length()%2 == 0) {
-                    if (stoneString.length() == 2) {
-                        newPlutoStones.add(Long.parseLong(String.valueOf(stoneString.charAt(0))));
-                        newPlutoStones.add(Long.parseLong(String.valueOf(stoneString.charAt(1))));
+                // System.out.println("New val " + plutoStone + " at time " + blinkTime);
+                String stoneString = String.valueOf(plutoStone);
+                HashMap<Integer, Long> innerMap = new HashMap<>();
+                Long bLong = 0L;
+                if (plutoStone == 0) {
+                    bLong = blink(1L, blinkTime - 1);
+                    innerMap.put(blinkTime, bLong);
+                    if (memo.containsKey(plutoStone)) {
+                        memo.get(plutoStone).put(blinkTime, bLong);
                     } else {
-                        newPlutoStones.add(Long.parseLong(stoneString.substring(0, stoneString.length()/2)));
-                        newPlutoStones.add(Long.parseLong(stoneString.substring(stoneString.length()/2)));
+                        memo.put(plutoStone, innerMap);
+                    }
+                } else if (stoneString.length()%2 == 0) {
+                    if (stoneString.length() == 2) {
+                        bLong = blink(Long.parseLong(String.valueOf(stoneString.charAt(0))), blinkTime - 1) +
+                                blink(Long.parseLong(String.valueOf(stoneString.charAt(1))), blinkTime - 1);
+                    } else {
+                        bLong = blink(Long.parseLong(stoneString.substring(0, stoneString.length()/2)), blinkTime - 1) +
+                                blink(Long.parseLong(stoneString.substring(stoneString.length()/2)), blinkTime - 1);
                     }
                 } else {
-                    newPlutoStones.add(plutoStones.get(stone) * 2024);
+                    bLong = blink(plutoStone * 2024, blinkTime - 1);
                 }
+                innerMap.put(blinkTime, bLong);
+                if (memo.containsKey(plutoStone)) {
+                    memo.get(plutoStone).put(blinkTime, bLong);
+                } else {
+                    memo.put(plutoStone, innerMap);
+                }
+                return memo.get(plutoStone).get(blinkTime);
             }
-            // System.out.println(newPlutoStones);
         }
-
-        return newPlutoStones; 
     }
-
-    //Part 2 (and I could have done this for part 1 tbh)
-    // public static Long numOfStone() {
-    //     return 0L;
-    // }
-
-    // public static ArrayList<Long> innerArray() {
-    //     ArrayList<Long> inner = new ArrayList<>();
-    //     return inner;
-    // }
 
     /*
      * Part 1
      * 60973 TOO LOW
      * 190865 CORRECT
+     * 6521701160 - 50 blinks
      */
 }
