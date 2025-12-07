1d39f560ba5b778acca76529903dfd27e3918f40
Cholo Galvez <240070477+cholavez-boop@users.noreply.github.com>
2025-12-08 02:52:48 +0800

    AoC 2025 Day 2 Part 2 ongoing

--- src/AoC_2025_02.java
+++ src/AoC_2025_02.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class AoC_2025_02 {
     public static Long sum = 0L;
     public static void main(String[] args) {
-        File myObj = new File("/Users/Lorenzo Galvez/Documents/Work Files/JustCoding/AdventOfCoding/AoCInputs/AOC2025_02.txt");
-        // File myObj = new File("D:/Real Life/Oracle/NonWork/AoC_2025/AdventOfCoding/AoCInputs/AOC2025_02.txt");
+        // File myObj = new File("/Users/Lorenzo Galvez/Documents/Work Files/JustCoding/AdventOfCoding/AoCInputs/AOC2025_02.txt");
+        File myObj = new File("D:/Real Life/Oracle/NonWork/AoC_2025/AdventOfCoding/AoCInputs/AOC2025_02.txt");
 
         ArrayList<String> numRanges = new ArrayList<>();
         try (Scanner myReader = new Scanner(myObj)) {
@@ -33,56 +33,53 @@ public class AoC_2025_02 {
         String[] fullRange = range.split("-");
         int startLen = fullRange[0].length();
         int endLen = fullRange[1].length();
-        // if (startLen % 2 == 0 || endLen % 2 == 0 || startLen != endLen) {
-            // if (startLen % 2 != 0) {
-            //     fullRange[0] = newStart(startLen);
-            // }
-            // if (endLen % 2 != 0) {
-            //     fullRange[1] = newEnd(endLen);
-            // }
         Long start = Long.parseLong(fullRange[0]);
         Long end = Long.parseLong(fullRange[1]);
-        Long val = 0L;
+        int secLen = 1;
         String section = null;
-
+        ArrayList<Long> validNums = new ArrayList<>();
         if (startLen == 1) {
             fullRange[0] = "10";
         }
-        if (startLen == 2) {
-            section = String.valueOf(fullRange[0].charAt(0));
-        }
-
-        String half = startLen > 2 ? fullRange[0].substring(0, startLen/2) : String.valueOf(fullRange[0].charAt(0));
-        System.out.println(fullRange[0] + " to " + fullRange[1] + " (" + half + ")");
-        int temp = Integer.parseInt(half);
-        while (val < end) {
-            String sTemp = String.valueOf(temp);
-            StringBuilder newVal = new StringBuilder(sTemp);
-            newVal.append(sTemp);
-            val = Long.parseLong(newVal.toString());
-            if (val <= end && val >= start) {
-                System.out.println(val + " < " + end);
-                sum += val;
+        do {
+            if (secLen == 1) {
+                section = String.valueOf(fullRange[0].charAt(0));
+            } else {
+                section = fullRange[0].substring(0, secLen-1);
             }
-            temp++;
+            boolean inRange = true;
+            while (inRange) {
+                StringBuilder newSec = new StringBuilder(section);
+                while (newSec.toString().length() < startLen) {
+                    newSec.append(section);
+                }
+                Long val = Long.parseLong(newSec.toString());
+                if (val <= end && val >= start && !validNums.contains(val)){
+                    System.out.println(range + " " + val);
+                    validNums.add(val);
+                }
+                while (newSec.toString().length() < endLen) {
+                    newSec.append(section);
+                }
+                Long secondVal = Long.parseLong(newSec.toString());
+                if (secondVal != val && secondVal <= end && secondVal >= start && !validNums.contains(secondVal)){
+                    System.out.println(range + " " + val);
+                    validNums.add(val);
+                }
+
+                if (val > end || secondVal > end) {
+                    inRange = false;
+                } else {
+                    section = String.valueOf(Integer.parseInt(section) + 1);
+                }
+            }
+            secLen++;
+        } while (section.length() <= endLen/2);
+
+        for (Long validNum : validNums) {
+            sum += validNum;
         }
     }
-
-    // public static String newStart(int stringLength) {
-    //     StringBuilder ns = new StringBuilder("1");
-    //     for (int s = 0; s < stringLength; s++) {
-    //         ns.append("0");
-    //     }
-    //     return ns.toString();
-    // }
-
-    // public static String newEnd(int stringLength) {
-    //     StringBuilder ne = new StringBuilder();
-    //     for (int e = 0; e < stringLength-1; e++) {
-    //         ne.append("9");
-    //     }
-    //     return ne.toString();
-    // }
 
     /*
      * RIGHT
