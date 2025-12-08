e2321ed0cc7883aa9e5452d2626f9f2cdd04963c
Cholo Galvez <240070477+cholavez-boop@users.noreply.github.com>
2025-12-08 16:30:11 +0800

    AoC 2025 Day 2 Part 2 solution

--- src/AoC_2025_02.java
+++ src/AoC_2025_02.java
@@ -31,53 +31,65 @@ public class AoC_2025_02 {
 
     public static void checkNum(String range) {
         String[] fullRange = range.split("-");
-        int startLen = fullRange[0].length();
-        int endLen = fullRange[1].length();
         Long start = Long.parseLong(fullRange[0]);
         Long end = Long.parseLong(fullRange[1]);
-        int secLen = 1;
-        String section = null;
-        ArrayList<Long> validNums = new ArrayList<>();
-        if (startLen == 1) {
-            fullRange[0] = "10";
-        }
-        do {
-            section = fullRange[0].substring(0, secLen);
-            Long val = 0L;
-
-            while (val <= end) {
-                val = generateLong(section, startLen, start, end);
-                if (val >= start && val <= end && !validNums.contains(val)) {
-                    // System.out.println(range + " " + val);
-                    validNums.add(val);
-                }
-
-                if (endLen > startLen) {
-                    val = generateLong(section, endLen, start, end);
-                    if (val >= start && val <= end && !validNums.contains(val)) {
-                        System.out.println(range + " " + val);
-                        validNums.add(val);
+        for (Long i = start; i <= end; i++) {
+            String numStr = String.valueOf(i);
+            int len = numStr.length();
+            boolean valid = false;
+            for (int secLen = 1; secLen <= len / 2; secLen++) {
+                if (len % secLen == 0) {
+                    String section = numStr.substring(0, secLen);
+                    StringBuilder builtNum = new StringBuilder();
+                    int repeatTimes = len / secLen;
+                    for (int j = 0; j < repeatTimes; j++) {
+                        builtNum.append(section);
+                    }
+                    if (builtNum.toString().equals(numStr)) {
+                        valid = true;
+                        break;
                     }
                 }
-                section = String.valueOf(Integer.parseInt(section) + 1);
             }
-
-            secLen++;
-        } while (secLen <= endLen/2);
-
-        for (Long validNum : validNums) {
-            sum += validNum;
+            if (valid) {
+                sum += i;
+            }
         }
+        
     }
 
-    public static Long generateLong(String section, int goalLen, Long start, Long end) {
-        StringBuilder newSec = new StringBuilder(section);
-        do {
-            newSec.append(section);
-        } while (newSec.toString().length() < goalLen);
-        System.out.println(start + "-" + end + " Generated: " + newSec.toString() + (start <= Long.parseLong(newSec.toString()) ? "" : " TOO LOW") + (Long.parseLong(newSec.toString()) <= end ? "" : " TOO HIGH"));
-        return Long.parseLong(newSec.toString());
-    }
+    // public static void checkNum(String range) {
+    //     String[] fullRange = range.split("-");
+    //     int endLen = fullRange[1].length();
+    //     Long start = Long.parseLong(fullRange[0]);
+    //     Long end = Long.parseLong(fullRange[1]);
+    //     int secLen = 1;
+    //     ArrayList<Long> validNums = new ArrayList<>();
+    //     do {
+    //         String section = fullRange[0].substring(0, secLen);
+    //         Long val = 0L;
+    //         while (val <= end) {
+    //             val = generateLong(section, fullRange[0].length());
+    //             if (val >= start && val <= end && !validNums.contains(val)) {
+    //                 System.out.println(range + " " + val);
+    //                 validNums.add(val);
+    //             }
+    //             section = String.valueOf(Integer.parseInt(section) + 1);
+    //         }
+    //         secLen++;
+    //     } while (secLen <= endLen/2);
+    //     for (Long validNum : validNums) {
+    //         sum += validNum;
+    //     }
+    // }
+
+    // public static Long generateLong(String section, int goalLen) {
+    //     StringBuilder newSec = new StringBuilder(section);
+    //     do {
+    //         newSec.append(section);
+    //     } while (newSec.toString().length() < goalLen);
+    //     return Long.parseLong(newSec.toString());
+    // }
 
     /*
      * RIGHT
@@ -87,5 +99,6 @@ public class AoC_2025_02 {
      * 39918681190 PART 1 TOO HIGH
      * 45753469068 PART 2 TOO LOW
      * 45753465280 Part 2 TOO LOW
+     * 45971524360 PART 2 TOO HIGH
      */
 }
